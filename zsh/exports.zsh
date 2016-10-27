e46226fe2501e13f866b02de0525b1fabbdae534
Tony Foerster <anthonymfoerster@gmail.com>
2016-10-26 21:13:12 -0500

    customize

---
+++ zsh/exports.zsh
@@ -0,0 +1,11 @@
+# enable aliasing in java apps
+
+# Setup terminal, and turn on colors
+export LSCOLORS=gxfxcxdxbxegedabagacad
+export CLICOLOR=1
+
+# Enable color in grep
+export GREP_OPTIONS='--color=auto' 
+export GREP_COLOR='3;33'
+
+export PAGER=less
