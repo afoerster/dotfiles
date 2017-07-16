282436e3b2fb008605c29d8dd6fac1b56320f714
Tony Foerster <anthonymfoerster@gmail.com>
2017-07-16 15:11:41 -0500

    Cleanup

--- bash/bash-it-config.sh
+++ bash/bash-it-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 bash-it enable completion ssh pip3 pip terraform dirs maven
-bash-it enable plugin tmux virtualenv less-pretty-cat docker extract
+bash-it enable plugin tmux virtualenv  docker extract
--- bash/install.sh
+++ bash/install.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it || true
 ~/.bash_it/install.sh --silent --no-modify-config
-ln -s bash_profile ~/.bash_profile
-source $CURRENT_DIR/bash-it-config.sh
+ln -s bash_profile ~/.bash_profile || true
+#source $CURRENT_DIR/bash-it-config.sh
