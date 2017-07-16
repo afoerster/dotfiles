a339993922cb8028d7a3f375c0c31c43e0974884
Tony Foerster <anthonymfoerster@gmail.com>
2017-07-16 13:54:00 -0500

    Softlink bash profile

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it || true
-cp bash_profile ~/.bash_profile
+lns -s bash_profile ~/.bash_profile
 ~/.bash_it/install.sh --silent --no-modify-config
 echo "source <($CURRENT_DIR/root.sh)" >> ~/.bash_profile
