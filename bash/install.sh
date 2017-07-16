7ea25519860516f7605b3e2a5514fff013354908
Tony Foerster <anthonymfoerster@gmail.com>
2017-07-16 15:23:52 -0500

    Cleanup

--- bash/install.sh
+++ bash/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -euo pipefail
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it || true
 ~/.bash_it/install.sh --silent --no-modify-config
 ln -s bash_profile ~/.bash_profile || true
-#source $CURRENT_DIR/bash-it-config.sh
+source ~/.bash_profile
+source $CURRENT_DIR/bash-it-config.sh
