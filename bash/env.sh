c9084c620c3a3da96fa805320462139d91e2b5f2
Tony Foerster <tony@phdata.io>
2017-07-17 06:51:09 -0500

    Fix broken symlink installer

--- bash/env.sh
+++ bash/env.sh
@@ -1,6 +1,6 @@
 export PATH=$HOME/.vimpkg/bin:$PATH
 export PATH=$HOME/.cargo/bin:$PATH
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
-export PAGER=most
-export MANPAGER=most
+export PAGER=less
+export MANPAGER=less
 
